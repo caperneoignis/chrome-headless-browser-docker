1fbf114dd5cddc0aa6a4db4b9c64e305f844c649
Lee Kirkland <weeman.kirkland@gmail.com>
2017-12-29 13:13:09 -0500

    added entry point
    added entry point, with dynamic settings.

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -2,16 +2,6 @@
 
 umask 022
 
-#do a string replace for configuration files for the web root. 
-if [[ "${APACHE_WEB_ROOT}" != "" ]]; then
-    sed -i "s#<<APACHE_WEB_ROOT>>#${APACHE_WEB_ROOT}#" /etc/apache2/sites-enabled/000-default.conf
-	sed -i "s#%%APACHE_WEB_ROOT%%#${APACHE_WEB_ROOT}#" /etc/apache2/apache2.conf
-else
-    #set a default if one is not present
-    APACHE_ROOT="/var/www/html"
-    sed -i "s#<<APACHE_WEB_ROOT>>#${APACHE_ROOT}#" /etc/apache2/sites-enabled/000-default.conf
-	sed -i "s#%%APACHE_WEB_ROOT%%#${APACHE_ROOT}#" /etc/apache2/apache2.conf
-fi
 if [[ "${RMT_DBG_ADDR}" == "" ]]; then
     RMT_DBG_ADDR=0.0.0.0
 fi
@@ -22,10 +12,4 @@ if [[ "${RMT_DBG_WIN_SIZE}" == "" ]]; then
     RMT_DBG_WIN_SIZE="1920,1080"
 fi
 #I want this to run in a sub shell
-/usr/bin/google-chrome-stable --disable-gpu --headless --remote-debugging-address=${RMT_DBG_ADDR} --remote-debugging-port=${RMT_DBG_PORT} --no-sandbox --window-size=${RMT_DBG_WIN_SIZE} &
-
-if [[ $# -eq 1 && $1 == "bash" ]]; then
-    $@
-else
-    exec "$@"
-fi
+/usr/bin/google-chrome-stable --disable-gpu --headless --remote-debugging-address=${RMT_DBG_ADDR} --remote-debugging-port=${RMT_DBG_PORT} --no-sandbox --window-size=${RMT_DBG_WIN_SIZE}
