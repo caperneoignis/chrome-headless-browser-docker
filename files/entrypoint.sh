935cc50bb4863f25ef7d63929cf3533a3a80ec50
Lee Kirkland <weeman.kirkland@gmail.com>
2017-12-29 17:12:43 -0500

    I want output to go to text file
    made program execution cleaner.

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -21,8 +21,15 @@ fi
 if [[ "${RMT_DBG_WIN_SIZE}" == "" ]]; then
     RMT_DBG_WIN_SIZE="1920,1080"
 fi
-#I want this to run in a sub shell
-/usr/bin/google-chrome-stable --disable-gpu --headless --remote-debugging-address=${RMT_DBG_ADDR} --remote-debugging-port=${RMT_DBG_PORT} --no-sandbox --window-size=${RMT_DBG_WIN_SIZE} &
+#I want this to run in a sub shell and output to go to txt file
+/usr/bin/google-chrome-stable \
+--disable-gpu \
+--headless \
+--remote-debugging-address=${RMT_DBG_ADDR} \
+--remote-debugging-port=${RMT_DBG_PORT} \
+--no-sandbox \
+--window-size=${RMT_DBG_WIN_SIZE} \
+> ${APACHE_WEB_ROOT}/output.txt &
 
 if [[ $# -eq 1 && $1 == "bash" ]]; then
     $@
