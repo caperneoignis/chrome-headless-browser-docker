4c98a34c103b8f110ab0e7aafd8be85701947de2
Lee Kirkland <weeman.kirkland@gmail.com>
2017-12-29 18:37:08 -0500

    fixed issue
    fixed issue

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -6,13 +6,11 @@ umask 022
 if [[ "${APACHE_WEB_ROOT}" != "" ]]; then
     sed -i "s#<<APACHE_WEB_ROOT>>#${APACHE_WEB_ROOT}#" /etc/apache2/sites-enabled/000-default.conf
 	sed -i "s#%%APACHE_WEB_ROOT%%#${APACHE_WEB_ROOT}#" /etc/apache2/apache2.conf
-	sed -i "s#<<SERVER_NAME>>#${SERVER_NAME}#" /etc/apache2/apache2.conf
 else
     #set a default if one is not present
     APACHE_ROOT="/var/www/html"
     sed -i "s#<<APACHE_WEB_ROOT>>#${APACHE_ROOT}#" /etc/apache2/sites-enabled/000-default.conf
 	sed -i "s#%%APACHE_WEB_ROOT%%#${APACHE_ROOT}#" /etc/apache2/apache2.conf
-	sed -i "s#<<SERVER_NAME>>#${SERVER_NAME}#" /etc/apache2/apache2.conf
 fi
 
 if [[ "${SERVER_NAME}" != "" ]]; then
@@ -39,8 +37,7 @@ fi
 --remote-debugging-address=${RMT_DBG_ADDR} \
 --remote-debugging-port=${RMT_DBG_PORT} \
 --no-sandbox \
---window-size=${RMT_DBG_WIN_SIZE} \
-> /dev/null &
+--window-size=${RMT_DBG_WIN_SIZE} > /dev/null 2>&1 &
 
 if [[ $# -eq 1 && $1 == "bash" ]]; then
     $@
